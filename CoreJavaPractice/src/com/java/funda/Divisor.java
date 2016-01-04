52f93af5e5aaaa1c1e9a5a472de5d45087fdc36a
SaiKoushik1314 <sai.koushik1314@gmail.com>
2016-01-03 21:30:22 -0500

    one

---
+++ CoreJavaPractice/src/com/java/funda/Divisor.java
@@ -0,0 +1,13 @@
+package com.java.funda;
+
+public class Divisor {
+	public static void main(String[] args) {
+		int a = 105;
+		int b = 2;
+		if (a % b == 0) {
+			System.out.println(a + " is divisible by " + b);
+		} else {
+			System.out.println(a + " is not divisible by " + b);
+		}
+	}
+}
